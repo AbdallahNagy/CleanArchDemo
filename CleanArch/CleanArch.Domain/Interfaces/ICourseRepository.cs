7b48bd5d5e7133e1ab20d045ab914d4f95bd89a3
abdallah nagy <abdallahnagy773@gmail.com>
2024-08-02 00:21:25 +0300

    create service and repo implementation

---
+++ CleanArch/CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,8 @@
+﻿using CleanArch.Domain.Models;
+
+namespace CleanArch.Domain.Interfaces;
+
+public interface ICourseRepository
+{
+    IEnumerable<Course> GetCourses();
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repository;
+
+public class CourseRepository : ICourseRepository
+{
+    private readonly UniversityDbContext _context;
+
+    public CourseRepository(UniversityDbContext context)
+    {
+        _context = context;
+    }
+    public IEnumerable<Course> GetCourses()
+    {
+        throw new NotImplementedException();
+    }
+}
