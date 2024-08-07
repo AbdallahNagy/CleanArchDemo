2ed937eab517acdd4d1b6954d660afca59c939e0
abdallah nagy <abdallahnagy773@gmail.com>
2024-08-08 00:13:10 +0300

    create domain core project

--- CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -1,6 +1,18 @@
-﻿namespace CleanArch.Infra.Bus;
+﻿using MediatR;
 
-public class InMemoryBus
+namespace CleanArch.Infra.Bus;
+
+public sealed class InMemoryBus : IMediatorHandler
 {
+    private readonly IMediator _mediator;
 
+    public InMemoryBus(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public Task SendCommand<T>(T command) where T : Command
+    {
+        return _mediator.Send(command);
+    }
 }
